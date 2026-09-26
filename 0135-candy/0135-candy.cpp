b96a96c7dd65b861cc8b03fd1365ba9732dad167
Harsh pareek <harshpareek809@gmail.com>
2026-09-27 00:36:17 +0530

    Time: 0 ms (100%), Space: 71.3 MB (49.42%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int count = 0;
+        vector<int>candy(ratings.size(),1);
+
+        for(int i = 1; i < ratings.size(); i++){
+             if(ratings[i] > ratings[i-1]){
+                candy[i] = candy[i-1] + 1;
+             }
+        }
+
+        for(int i = ratings.size()-2; i >= 0; i--) {
+            if(ratings[i] > ratings[i+1]) {
+                candy[i] = max(candy[i], candy[i+1] + 1);
+            }
+        }
+
+        for(int i = 0; i < candy.size(); i++){
+            count += candy[i];
+        }
+        return count;
+    }
+};
